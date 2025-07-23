b84f269ddcb11387456b4cbf0bf3b50ac942b836
2003-Miguel <156967679+2003-Miguel@users.noreply.github.com>
2025-07-23 03:11:51 -0400

    Update Crear_BD_RH_DB.sql

--- Scripts_SQL/Crear_BD_RH_DB.sql
+++ Scripts_SQL/Crear_BD_RH_DB.sql
@@ -1,9 +1,1 @@
 CREATE DATABASE RH_DB;
-
-CREATE TABLE Empleados (
-    EmpleadoID INT PRIMARY KEY,
-    Nombre NVARCHAR(100),
-    Departamento NVARCHAR(100),
-    FechaIngreso DATE,
-    Salario DECIMAL(10, 2)
-);
