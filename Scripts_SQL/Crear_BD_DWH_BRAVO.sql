1650b3ea205de76e1a85872b18fd69744c71e5bb
2003-Miguel <156967679+2003-Miguel@users.noreply.github.com>
2025-07-23 04:00:25 -0400

    Update Crear_BD_DWH_BRAVO.sql

--- Scripts_SQL/Crear_BD_DWH_BRAVO.sql
+++ Scripts_SQL/Crear_BD_DWH_BRAVO.sql
@@ -1,1 +1,11 @@
 CREATE DATABASE DWH_BRAVO;
+
+CREATE TABLE DimEmpleado (
+    EmpleadoKey INT IDENTITY(1,1) PRIMARY KEY,
+    EmpleadoID INT,
+    Nombre NVARCHAR(100),
+    Departamento NVARCHAR(100),
+    FechaIngreso DATE,
+    Salario DECIMAL(10, 2),
+    FechaCarga DATETIME
+);
